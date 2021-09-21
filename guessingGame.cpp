b03c30349381afac929ffaf5d46b9bf8d20b2d57
U-BSD\477552 <477552@SHS-10D9C002C.beaverton.k12.or.us>
2021-09-21 11:57:42 -0700

    updated guessingame

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -15,11 +15,11 @@ using namespace std;
 
 int main()
 {
-  cout << "This is Guessing Game? (probably)" << endl;;
+  cout << "This is Guessing Game? (probably) Insert a guess to start" << endl;;
   srand (time(NULL));
   int randomNum = rand() % 100 +1; //random number generator
   int userInput = 0;
-  int guesses = 1;
+  int guesses = 0;
   bool playAgain = true;
   //cout << randomNum << endl;
   
@@ -27,9 +27,25 @@ int main()
     {
       
       cin >> userInput;
-      cout << "Your input number was " << userInput << endl;
+      guesses++;
+      //cout << "Your input number was " << userInput << endl;
+      if (userInput > randomNum) { //user input was higher than random
+	cout << "Your input number was " << userInput << ", and it was HIGHER" << endl;
+      } else if (userInput < randomNum){//user input was lower than random
+	cout << "Your input number was " << userInput << ", and it was LOWER" << endl;
+      } else { //random num must = input right?
+	cout << "Your input number was CORRECT! " << "You used: " << guesses << " guesses.  " << "Would you like to play again? 1 -> play again, 0 -> end game" << endl; //too lazy so just used a 1 for yes and 0 for no
+	  cin >> userInput;
+	if (userInput == 1) { //reset all vars and numbers
+	    playAgain = true;
+	    randomNum = rand() % 100 + 1;
+	    guesses = 0;
+	    cout << "This is Guessing Game? (probably) Insert a guess to start" << endl;;
+	} else { //exit the game
+	    playAgain = false;
+	  }
+      }
       
-      playAgain = false;
     }
   
   return 0;
