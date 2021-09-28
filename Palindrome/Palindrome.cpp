1f60107f98768d1bf1b95733e96bcd943c31b2fd
U-BSD\477552 <477552@SHS-10DEC49DF.beaverton.k12.or.us>
2021-09-28 13:06:36 -0700

    added all caps and removed spaced added count

--- Palindrome/Palindrome.cpp
+++ Palindrome/Palindrome.cpp
@@ -4,24 +4,62 @@ Date Created: 9/22/21
 Last Modified: 9/22/21
 Palindrome Assignment
 
+Sources:
+https://www.geeksforgeeks.org/cin-get-in-c-with-examples/
+http://www.cplusplus.com/reference/cstring/strlen/
+https://www.asciitable.com/
+https://www.programiz.com/cpp-programming/library-function/cctype/toupper
+https://www.geeksforgeeks.org/remove-spaces-from-a-given-string/
+
 
 */
 #include <iostream>
 #include <cstring>
+#include <cctype>
 
 using namespace std;
+//TAKEN FROM: https://www.geeksforgeeks.org/remove-spaces-from-a-given-string/
+//has been adapted to remove all non letter characters
+// An efficient C++ program to remove all spaces
+// from a string
+#include <iostream>
+using namespace std;
+ 
+// Function to remove all spaces from a given string
+void removeSpaces(char *str)
+{
+    // To keep track of non-space character count
+    int count = 0;
+ 
+    // Traverse the given string. If current character
+    // is not space, then place it at index 'count++'
+    for (int i = 0; str[i]; i++)
+      if (str[i] > 'A' || str[i] < 'Z')
+            str[count++] = str[i]; // here count is
+                                   // incremented
+    str[count] = '\0';
+}
+
 
 int main() {
+
+  
+	char input[81]; //reads in 80 characters
+	char reverseInput[81];
+	cin.getline(input, 81, '\n'); 
 	
-	char input[80];
-	char reverseInput[20];
-	cin.get(input, 80); 
-	
+	cout << "You entered: " << input << endl;
+	cout << "It has a length of " << strlen(input) << endl;
+	int inputLength = strlen(input);
+	for (int i = 0; i < inputLength; i++) {
+	  char ch = input[i];
+	  input[i] = toupper(ch);
+
+
+	}
+	removeSpaces(input);
 	cout << input << endl;
-	int count = 0;
-
-
-
+        
 
 
 
@@ -37,14 +75,6 @@ int main() {
 	  cout << "Not A Palindrome" << endl; 
 
 	}
-	/*for (int i = 0; i < 20; i++) {
-	  if (str[i] != '\0'){
-	    str2[count] = str[i];
-	    count++;
-	  }
-
-
-	  }*/
 	
 	
 	return 0;
