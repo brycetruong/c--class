6b48f8f78379f6ccdd88384b0eb8f5460964bfe8
U-BSD\477552 <477552@SHS-10D9C06DD.beaverton.k12.or.us>
2021-10-26 12:23:54 -0700

    FINALLY

--- studentList/studentList.cpp
+++ studentList/studentList.cpp
@@ -1,9 +1,18 @@
 /*
 Author: Bryce Truong
 Date Created: 10/18/21
-Last Modified: 10/18/21
+Last Modified: 10/26/21
+
+This is a program that reads out student lists.
 
 
+Sources:
+https://www.geeksforgeeks.org/vector-in-cpp-stl/
+https://www.geeksforgeeks.org/vectorat-vectorswap-c-stl/
+https://www.geeksforgeeks.org/vectorbegin-vectorend-c-stl/
+https://java2blog.com/vector-of-structs-in-cpp/
+
+Kubir's vast knowledge banks ;)
 
 */
 #include <iostream>
@@ -13,6 +22,7 @@ Last Modified: 10/18/21
 #include <array>
 #include <stdio.h>
 #include <vector>
+#include <iterator>
 
 using namespace std;
 
@@ -31,22 +41,50 @@ int main() {
   
   vector<Student*> myVec;
   while (stillRunning == true) {
+    cout << "Enter mode: \n\t-ADD\n\t-DELETE\n\t-PRINT\n\t-QUIT" << endl;
     cin.getline(input, 10, '\n');
     if (strcmp(input, "ADD") == 0) {
-      cout << "Add Mode\n--------\nEnter Student Name: " << endl;
+      cout << "Add Mode\n--------\nEnter Student Firstname: " << endl;
+      Student* temp = new Student();
+      
+      cout << "Enter Student Firstname: " << endl;
       cin.getline(input, 10, '\n');
-      myVec.push_back(new Student());
-      myVec[0]->fname = input;
-      cout << myVec[0]->fname << endl;
+      strcpy(temp -> fname, input);
+
+      cout << "Enter Student Lastname: " << endl;
+      cin.getline(input, 10, '\n');
+      strcpy(temp -> lname, input);
+      
+      //cout << temp.uid << endl;
+
+      cout << "Enter StudentID: " << endl;
+      cin.getline(input, 10, '\n');
+      strcpy(temp -> uid, input);
+
+      cout << "Enter GPA: " << endl;
+      cin.getline(input, 10, '\n');
+      strcpy(temp -> gpa, input);
+      
+      myVec.push_back(temp);
+      
+      //cout << myVec.at(0) -> fname << endl;
     } else if (strcmp(input, "DELETE") == 0) {
       cout << "DELETE" << endl;
     } else if (strcmp(input, "QUIT") == 0) {
       cout << "QUIT" << endl;
       stillRunning = false;
     } else if (strcmp(input, "PRINT") == 0) {
-    cout << "PRINT" << endl;
+      //cout << "PRINT" << endl;
+      for (int i = 0; i < myVec.size(); i++) {
+	cout << "Firstname: " << myVec.at(i) -> fname << endl;
+	cout << "\tLirstname: " << myVec.at(i) -> lname << endl;
+	cout << "\tStudentID: " << myVec.at(i) -> uid << endl;
+	cout << "\tGPA: " << myVec.at(i) -> gpa << endl;
+	cout << endl;
+      }
+
     } else {
-      cout << "Plese entr a vald imkapoot" << endl;
+      cout << "Plese entr a vald impoot" << endl;
     }
   }
   
