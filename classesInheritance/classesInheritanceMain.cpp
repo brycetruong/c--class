939f2923f0e1631e7258cb9c2141c18d562ff458
PersonLastname <75811760+PersonLastname@users.noreply.github.com>
2021-11-19 12:54:34 -0800

    Update classesInheritanceMain.cpp

--- classesInheritance/classesInheritanceMain.cpp
+++ classesInheritance/classesInheritanceMain.cpp
@@ -86,7 +86,7 @@ int main() {
       cout << mediaList[0]->getYear() << endl;
       
       cout << mediaList[1]->getYear() << endl;
-      cout << ((videoGames*)myVec.at(0))->getRate() << endl;
+      cout << ((videoGames*)mediaList[1])->getRate() << endl;
       
       cout << "Input not recognized..." << endl;
     }
