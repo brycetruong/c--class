8785f11403efa0b79fda8d715c10034cb8db8a76
PersonLastname <75811760+PersonLastname@users.noreply.github.com>
2021-11-29 12:04:38 -0800

    Update media.cpp

--- classesInheritance/media.cpp
+++ classesInheritance/media.cpp
@@ -11,6 +11,10 @@ Last Modified: 11/5/21
 #include <stdio.h>
 #include "media.h"
 
+const int MEDIA = 0;
+const int VG = 1; //videogame
+const int MS = 2; //music
+const int MV = 3; //movie
 
 using namespace std;
 
@@ -18,12 +22,14 @@ media::media(){
   //strcpy(title, newTitle);
   title = new char[20];
   year = 2000;
+  type = MEDIA;
 }
 
 media::media(char* newTitle, int newYear){
   title = new char[20];
   strcpy(title, newTitle);
   year = newYear;
+  type = MEDIA;
 }
 
 char* media::getTitle(){
@@ -32,4 +38,8 @@ char* media::getTitle(){
 
 int media::getYear(){
   return year;
+}
+
+int media::getType(){
+ return type; 
 }
