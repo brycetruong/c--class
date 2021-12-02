9fd8329e33ca73279aebf34ae619adec20b4d99a
PersonLastname <75811760+PersonLastname@users.noreply.github.com>
2021-12-01 17:26:07 -0800

    Update classesInheritanceMain.cpp

--- classesInheritance/classesInheritanceMain.cpp
+++ classesInheritance/classesInheritanceMain.cpp
@@ -189,13 +189,37 @@ int main() {
         
         for (int i = 0; i < myVec.size(); i++) {
           if (myVec.at(i) -> getYear() == atoi(input)) {
-            cout << "-\t-\t-\t-\t" << endl;
-            cout << "Title: " << myVec.at(i) -> getTitle() << endl;
-            cout << "Year: " << myVec.at(i) -> getYear() << endl;
-            cout << "-\t-\t-\t-\t" << endl;
-            cout << endl;
+            if (myVec.at(i) -> getType() == VG) {
+              cout << "-\t-\t-\t-\t" << endl;
+              cout << "Media Type: VideoGame" << endl;
+              cout << "Title: " << ((videoGames*)myVec.at(i)) -> getTitle() << endl;
+              cout << "Year: " << ((videoGames*)myVec.at(i)) -> getYear() << endl;
+              cout << "Publisher: " << ((videoGames*)myVec.at(i)) -> getPub() << endl;
+              cout << "Rating: " << ((videoGames*)myVec.at(i)) -> getRate() << endl;
+              cout << "-\t-\t-\t-\t" << endl;
+              cout << endl;
+            } else if (myVec.at(i) -> getType() == MS) {
+              cout << "-\t-\t-\t-\t" << endl;
+              cout << "Media Type: Music" << endl;
+              cout << "Title: " << ((music*)myVec.at(i)) -> getTitle() << endl;
+              cout << "Year: " << ((music*)myVec.at(i)) -> getYear() << endl;
+              cout << "Publisher: " << ((music*)myVec.at(i)) -> getPub() << endl;
+              cout << "Artist: " << ((music*)myVec.at(i)) -> getArt() << endl;
+              cout << "Duration: " << ((music*)myVec.at(i)) -> getDuration() << endl;
+              cout << "-\t-\t-\t-\t" << endl;
+              cout << endl;
+            } else if (myVec.at(i) -> getType() == MV) {
+              cout << "-\t-\t-\t-\t" << endl;
+              cout << "Media Type: Movie" << endl;
+              cout << "Title: " << ((movie*)myVec.at(i)) -> getTitle() << endl;
+              cout << "Year: " << ((movie*)myVec.at(i)) -> getYear() << endl;
+              cout << "Director: " << ((movie*)myVec.at(i)) -> getDir() << endl;
+              cout << "Rating: " << ((movie*)myVec.at(i)) -> getRate() << endl;
+              cout << "Duration: " << ((movie*)myVec.at(i)) -> getDuration() << endl;
+              cout << "-\t-\t-\t-\t" << endl;
+              cout << endl;
+            }
           }
-          
         }
       } else {
         cout << "Input not recognized..." << endl;
