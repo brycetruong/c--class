e04f3b71b2fae81c066ddedf0dc6fbba2285b401
PersonLastname <75811760+PersonLastname@users.noreply.github.com>
2021-12-15 11:56:45 -0800

    Update room.cpp

--- zuul/room.cpp
+++ zuul/room.cpp
@@ -7,6 +7,8 @@ Last Modified: 12/9/21
 #include <iostream>
 #include <cstring>
 #include <stdio.h>
+#include <vector>
+#include <iterator>
 #include "room.h"
 
 using namespace std;
@@ -21,11 +23,25 @@ room::~room(){
   delete[] desc;
 }
 
-room::room(char* newDesc){
+room::room(char* newDesc) {
   desc = new char[80];
   strcpy(desc, newDesc);
 }
 
-char* room::getDesc(){
+char* room::getDesc() {
   return desc;
+}
+
+void room::addItem(item* newItem) {
+  roomInv.push_back(newItem);
+}
+
+void room::remItem() {
+  
+}
+
+void room::printItems() {
+ for (int i = 0; i < roomInv.size(); i++) {
+   cout << roomInv.at(i)->getName() << endl;
+ }
 }
