a7289a061c3d03b8034b8840361050cd03ee82d4
PersonLastname <75811760+PersonLastname@users.noreply.github.com>
2022-01-04 11:30:49 -0800

    Update main.cpp

--- zuul/main.cpp
+++ zuul/main.cpp
@@ -44,8 +44,8 @@ int main() {
   strcpy(TR1Desc, "testRoom1 desc?");
   char* TR1Name = new char[20];
   strcpy(TR1Desc, "testRoom1");
-  testRoom1 = new room(TR1Desc, TR1Name);
-  testRoom2 = new room(TR1Desc, TR1Name);
+  room* testRoom1 = new room(TR1Desc, TR1Name);
+  room* testRoom2 = new room(TR1Desc, TR1Name);
   
   testRoom1->setExit(NORTH, testRoom2*);
     
