d684281dd88bf3c4e9881a8024c88bf2083bae51
PersonLastname <75811760+PersonLastname@users.noreply.github.com>
2022-01-21 12:00:15 -0800

    Update Student.cpp

--- linkedListsPart2/Student.cpp
+++ linkedListsPart2/Student.cpp
@@ -1,24 +1,45 @@
 #include "Student.h"
+
+//Author: Tom
+//Co Author: Bryce
+
 Student::Student(){
 
 }
 Student::~Student(){
 
 }
+
+Student::Student(char* newFname, char* newLname, int newID, double newGPA) {
+  fname = new char[80];
+  strcpy(fname, newFname);
+
+  fname = new char[80];
+  strcpy(fname, newFname);
+
+  ID = newID;
+
+  GPA = newGPA;
+}
+
 void Student::setStudentfName(char* newfName){
   fname = new char[50];
   strcpy(fname, newfName);
 }
+
 char* Student::getStudentfName(){
   return fname;
 }
+
 void Student::setStudentlName(char* newlName){
   lname = new char[50];
   strcpy(lname, newlName);
 }
+
 char* Student::getStudentlName(){
   return lname;
 }
+
 void Student::setStudentID(int newID){
   ID = newID;
 }
