07c72889b913497484f3296a42853bd25dd4d694
PersonLastname <75811760+PersonLastname@users.noreply.github.com>
2022-01-21 12:16:37 -0800

    Update main.cpp

--- linkedListsPart2/main.cpp
+++ linkedListsPart2/main.cpp
@@ -47,7 +47,7 @@ int main() {
   */
   
   while (running) {
-    cout << "Welcome to the menu. Please select one of the following:\nADD\tDELETE\tSEARCH\tQUIT" << endl;
+    cout << "Welcome to the menu. Please select one of the following:\nADD\tDELETE\tPRINT\tQUIT" << endl;
     cin.getline(input, 50, '\n');
     if (strcmp(input, "ADD") == 0) {
       
