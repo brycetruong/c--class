d4666b9f75773934bf344a31ff2a81c73ff33469
PersonLastname <75811760+PersonLastname@users.noreply.github.com>
2022-01-27 12:04:40 -0800

    Update main.cpp

--- linkedListsPart2/main.cpp
+++ linkedListsPart2/main.cpp
@@ -127,8 +127,9 @@ void print(Node* next) {
 
 void deleteNode(Node*& head, Node* current, int id) {
   if (current != NULL) {
+    cout << "!null" << endl;
     if (current->getNext()->getStudent()->getStudentID() == id) {
-      
+      cout << "match" <<endl;
       Node* temp = current->getNext();
       if (current->getNext()->getNext() != NULL) { //if two spaces forwards is not null
         current->setNext(current->getNext()->getNext()); //repair the link
